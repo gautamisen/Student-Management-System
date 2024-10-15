4efcaea0320e37ef7c03878f5614acba38a8616f
gautamisen <sengautami930@gmail.com>
2024-10-15 21:48:14 +0530

    Init and Complete project

---
+++ src/main/java/com/sms/StudentMagagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMagagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMagagementSystemApplication.class, args);
+	}
+
+}
